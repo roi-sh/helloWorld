4bd7b2c6aa650845870d3235f8a1f81ced31fc70
roi <roish123456789@gmail.com>
2025-04-14 15:48:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // is a .NET method that returns information about the executable assembly that 
+        // was first loaded when the application started (the "entry point" assembly).
+        var assembly = Assembly.GetEntryAssembly();
+        
+        if (assembly?.GetName()?.Version is Version version)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Version: {version}");
+        }
+        else
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Version: Unknown");
+        }
+    }
+}
